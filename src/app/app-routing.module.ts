ad1a1b255d4a5511a9065e7295d92af43d21af5c
Jonas Vercammen2 <jonas.vercammen@vente-exclusive.com>
2019-10-25 14:41:29 +0200

    feat: lazy load home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview/overview.component';
+import { RouterModule } from '@angular/router';
 
-
+const routes = [
+  {
+    path: '',
+    component: OverviewComponent,
+  },
+];
 
 @NgModule({
   declarations: [OverviewComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes), CommonModule],
+  exports: [RouterModule],
 })
-export class HomeModule { }
+export class HomeModule {}
