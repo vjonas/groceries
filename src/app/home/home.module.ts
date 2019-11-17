845a02e642eef0b4b63f4620787d8ef1c7a6420d
jonasv <jonasvercammen7@gmail.com>
2019-11-17 04:05:20 +0000

    feat: add home components and scripts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview/overview.component';
 import { RouterModule } from '@angular/router';
+import { GroceriesComponent } from './overview/groceries/groceries.component';
 
 const routes = [
   {
     path: '',
     component: OverviewComponent,
   },
+  {
+    path: 'groceries',
+    component: GroceriesComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [OverviewComponent],
+  declarations: [OverviewComponent, GroceriesComponent],
   imports: [RouterModule.forChild(routes), CommonModule],
   exports: [RouterModule],
 })
